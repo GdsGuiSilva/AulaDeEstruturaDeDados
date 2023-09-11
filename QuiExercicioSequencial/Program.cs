85acfb5e512203bc8efacbe8463728607efad9b4
guilherme.silva <guilherme.silva@DESKTOP-33VB2LT>
2023-09-11 11:07:43 -0300

    Adicionar arquivos de projeto.

---
+++ QuiExercicioSequencial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuiExercicioSequencial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome : ");
+            String nome = Console.ReadLine();
+            Console.Write("Olá {0} ", nome);
+        }
+    }
+}
---
+++ SegExercicioSequencial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegExercicioSequencial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome :");
+            String nome = Console.ReadLine();
+            Console.Write("Digite sua idade :");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Seu nome é {0} com {1} anos de idade",nome,idade);
+        }
+    }
+}
